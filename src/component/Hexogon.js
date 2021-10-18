8be53b0203863b0a0639a6736b01c57621c2314d
Ihsan <ihsan9985@yandex.ru>
2021-10-18 22:55:01 +0300

    UpDate

--- src/component/Hexogon.js
+++ src/component/Hexogon.js
@@ -3,28 +3,20 @@ import {InfoBlocks} from "./InfoBlocks";
 import '../styles/main.css'
 
 export const Hexagon = () => {
-    const ii = InfoBlocks.filter((i )=> i.date === '26 июня')
+    const ii = InfoBlocks.filter((i) => i.date === '26 июня')
     const [isActive, setActive] = useState(false);
     const handleToggle = () => {
         setActive(!isActive);
     }
     return (
-                <>
-                    <div className={`roundHex hexagon_0 ${isActive ? "active" : ""}`} onClick={handleToggle}>
-                        <ul>
-                        {ii.map((info) =>
-                                <li key={info.place}>
-                                    {info.date}
-                                    <br/>
-                                    {info.place}
-                                    <br/>
-                                    {info.time}
-                                    <br/>
-                                </li>
-                            )}
-                        </ul>
-                    </div>
-                </>
-
+        <div className={`roundHex hexagon_0 ${isActive ? "active" : ""}`} onClick={handleToggle}>
+            {ii.map((info) =>
+                <ul key={info.date}>
+                    <li key={info.date}>{info.date}</li>
+                    <li key={info.place}>{info.place}</li>
+                    <li key={info.time}>{info.time}</li>
+                </ul>
+            )}
+        </div>
     )
 }
